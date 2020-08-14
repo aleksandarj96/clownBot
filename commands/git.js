6256dcad1e78f713ecab003bdb1b12c0bc844c1a
Alex <jeal1796@student.ju.se>
2020-08-14 21:12:22 +0200

    added git link

--- commands/git.js
+++ commands/git.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'git',
     descpription: "sends git link",
     execute(message, args){
-        message.channel.send("git link should be here")
+        message.channel.send("https://github.com/aleksandarj96/clownBot")
     }
 }
